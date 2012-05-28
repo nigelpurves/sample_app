38ab9bd9a78349223f22d24c10e4d9d1d158d486
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-05-28 11:52:04 +0100

    microposts done to end of 443

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   before_filter :authenticate
   
   def create
-    @micropost  = current_user.microposts.build(params[:micropost])
+    @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_path
